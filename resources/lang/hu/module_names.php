c09543a988906b18aaa78595807267630603bf35
gzs079 <giczi.zsolt@sze.hu>
2024-12-29 16:31:47 +0100

    magyar feliratok frissítése.... ANGOL FORDÍTÁS HIÁNYZIK

--- resources/lang/hu/module_names.php
+++ resources/lang/hu/module_names.php
@@ -1,20 +1,56 @@
 <?php
 
 return [
-  'navigation_groups' => [
-    'administration' => 'Adattáblák kezelése',
-  ],
-  'accreditedsamplingstatus' => [
-    'label' => 'Akkreditált mintavétel státusza',
-    'plural_label' => 'Akkreditált mintavétel státuszok',
-  ],
-  'humvimodule' => [
-    'label' => 'HUMVI modul',
-    'plural_label' => 'HUMVI modulok',
-  ],
-  'humviresponsible' => [
-    'label' => 'HUMVI felelős',
-    'plural_label' => 'HUMVI felelősök',
-  ],
-
+    'navigation_groups' => [
+        'administration' => 'Adattáblák kezelése',
+        'watersamples' => 'Vízminták kezelése',
+    ],
+    'accreditedsamplingstatus' => [
+        'label' => 'Akkreditált mintavétel státusza',
+        'plural_label' => 'Akkreditált mintavétel státuszok',
+    ],
+    'humvimodule' => [
+        'label' => 'HUMVI modul',
+        'plural_label' => 'HUMVI modulok',
+    ],
+    'humviresponsible' => [
+        'label' => 'HUMVI felelős',
+        'plural_label' => 'HUMVI felelősök',
+    ],
+    'drinkingwatersamples' => [
+        'label' => 'Ivóvízminta',
+        'plural_label' => 'Ivóvízminták',
+    ],
+    'samplingtype'=> [
+        'label' => 'Mintavétel típusa',
+        'plural_label' => 'Mintavétel típusok',
+    ],
+    'laboratory'=> [
+        'label' => 'Vizsgálólaboratórium',
+        'plural_label' => 'Vizsgálólaboratóriumok',
+    ],
+    'samplingreason'=> [
+        'label' => 'Mintavétel oka',
+        'plural_label' => 'Mintavétel okok',
+    ],
+    'sampler'=> [
+        'label' => 'Mintavevő',
+        'plural_label' => 'Mintavevők',
+    ],
+    'unit'=> [
+        'label' => 'Mértékegység',
+        'plural_label' => 'Mértékegységek',
+    ],
+    'samplingsite'=> [
+        'label' => 'Mintavételi hely',
+        'plural_label' => 'Mintavételi helyek',
+    ],
+    'parameter'=> [
+        'label' => 'Vizsgálati paraméter',
+        'plural_label' => 'Vizsgálati paraméterek',
+    ],
+    'result'=> [
+        'label' => 'Vizsgálati eredmény',
+        'plural_label' => 'Vizsgálati eredmények',
+    ],
 ];
