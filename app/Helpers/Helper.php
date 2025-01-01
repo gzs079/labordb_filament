c0d78254a9b4202571a87d5d3ff5f21fb5a41266
gzs079 <giczi.zsolt@sze.hu>
2025-01-01 19:28:11 +0100

    Grafikus widgetekhez színpaletta függvények előre definiált listából és darabszám alapján színkörből

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -45,3 +45,36 @@ if (! function_exists('calculate_valueassigned')) {
         }
     }
 }
+
+if (! function_exists('generateHSLColor')) {
+    function generateHSLColor($index, $total) {
+        $hue = ($index / $total) * 360;  // 360 fokos színkör, elosztva az adatok számával
+        return "hsl($hue, 70%, 50%)";  // Képletes HSL szín
+    }
+}
+
+if (! function_exists('generateColor')) {
+    function generateColor($index) {
+        $colors = [
+            'rgb(255, 99, 132)',
+            'rgb(54, 162, 235)',
+            'rgb(255, 205, 86)',
+            'rgb(75, 192, 192)',
+            'rgb(153, 102, 255)',
+            'rgb(255, 159, 64)',
+            'rgb(100, 150, 200)',
+            'rgb(255, 0, 255)',
+            'rgb(0, 255, 0)',
+            'rgb(0, 0, 255)',
+            'rgb(255, 165, 0)',
+            'rgb(128, 0, 128)',
+            'rgb(0, 255, 255)',
+            'rgb(255, 255, 0)',
+            'rgb(0, 0, 0)',
+        ];
+        return [
+            'color' => $colors[$index],
+            'total' => count($colors),
+        ];
+    }
+}
