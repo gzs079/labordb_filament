3d6a8cd1c283c82f81f8f60cd2d7ec36ac85743f
gzs079 <giczi.zsolt@sze.hu>
2025-01-02 16:12:31 +0100

    Dashboard widget felirat egységesítése -> 'plural' törlése

--- app/Filament/Widgets/TablesOverview.php
+++ app/Filament/Widgets/TablesOverview.php
@@ -18,11 +18,11 @@ class TablesOverview extends BaseWidget
     protected function getStats(): array
     {
         return [
-            Stat::make('Humvimodule', Humvimodule::query()->count())->label(__('module_names.humvimodule.plural_label')),
-            Stat::make('Humviresponsible', Humviresponsible::query()->count())->label(__('module_names.humviresponsible.plural_label')),
-            Stat::make('Samplingreason', Samplingreason::query()->count())->label(__('module_names.samplingreason.plural_label')),
-            Stat::make('Accreditedsamplingstatus', Accreditedsamplingstatus::query()->count())->label(__('module_names.accreditedsamplingstatus.plural_label')),
-            Stat::make('Samplingtype', Samplingtype::query()->count())->label(__('module_names.samplingtype.plural_label')),
+            Stat::make('Humvimodule', Humvimodule::query()->count())->label(__('module_names.humvimodule.label')),
+            Stat::make('Humviresponsible', Humviresponsible::query()->count())->label(__('module_names.humviresponsible.label')),
+            Stat::make('Samplingreason', Samplingreason::query()->count())->label(__('module_names.samplingreason.label')),
+            Stat::make('Accreditedsamplingstatus', Accreditedsamplingstatus::query()->count())->label(__('module_names.accreditedsamplingstatus.label')),
+            Stat::make('Samplingtype', Samplingtype::query()->count())->label(__('module_names.samplingtype.label')),
         ];
     }
 }
